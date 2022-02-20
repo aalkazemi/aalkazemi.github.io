e4ae1ecb4c2857a8cece99c12ce1dca62f1592c2
aalkazemi <65915845+aalkazemi@users.noreply.github.com>
2022-02-20 22:25:22 +0000

    update

--- script/pdf.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    document.getElementById("send")
-        .addEventListener("click",()=>{
-            const email = this.document.getElementById("send")
-            console.log(email);
-            console.log(window);
-            html2pdf().from(email).save();
-    })
-}
